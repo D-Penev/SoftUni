393edb012f57035f61c2f61ed938da3f6aeee999
Mitko <predator131@mail.bg>
2020-02-28 15:19:32 +0200

    Upload exercise

---
+++ Shit for C#/C# OOP/Encapsulation Exercises/03 - Shopping Spree/Person.cs
@@ -0,0 +1,76 @@
+﻿namespace EncapsulationExercises
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Person
+    {
+        private string name;
+
+        private decimal money;
+
+        private List<Product> products;
+
+        public Person(string name, decimal money)
+        {
+            this.Name = name;
+            this.Money = money;
+            this.Products = new List<Product>();
+        }
+
+        public string Name
+        {
+            get => this.name;
+            private set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+                this.name = value;
+            }
+        }
+        public decimal Money
+        {
+            get => this.money;
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+                this.money = value;
+            }
+        }
+
+        public List<Product> Products
+        {
+            get => this.products;
+            private set
+            {
+                this.products = value;
+            }
+        }
+
+        public void BuyProduct(Product product)
+        {
+            if (this.Money >= product.Cost)
+            {
+                this.Money -= product.Cost;
+                this.Products.Add(product);
+                Console.WriteLine($"{this.Name} bought {product.Name}");
+            }
+            else
+            {
+                Console.WriteLine($"{this.Name} can't afford {product.Name}");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Name}";
+        }
+    }
+}
---
+++ Shit for C#/C# OOP/Encapsulation Exercises/03 - Shopping Spree/Program.cs
@@ -0,0 +1,98 @@
+﻿namespace EncapsulationExercises
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+    public class StartUp
+    {
+        public static void Main()
+        {
+            var listOfPeople = new List<Person>();
+            var listOfProducts = new List<Product>();
+
+            string[] personData = Console.ReadLine().Split(new string[] { "=", ";" }, StringSplitOptions.RemoveEmptyEntries).ToArray();
+            string[] productData = Console.ReadLine().Split(new string[] { "=", ";" }, StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            decimal currentMoney = 0;
+            string currentName = string.Empty;
+            foreach (var item in personData)
+            {
+                if (!Decimal.TryParse(item,out currentMoney))
+                {
+                    currentName = item;
+                }
+                else
+                {
+                    try
+                    {
+                        var currentPerson = new Person(currentName, currentMoney);
+                        listOfPeople.Add(currentPerson);
+                    }
+                    catch (ArgumentException maikati)
+                    {
+
+                        Console.WriteLine(maikati.Message);
+                        return;
+                    }
+                }
+                
+            }
+
+            string productName = string.Empty;
+            decimal productCost = 0m;
+            foreach (var item in productData)
+            {
+                if (!Decimal.TryParse(item,out productCost))
+                {
+                    productName = item;
+                }
+                else
+                {
+                    try
+                    {
+                        var product = new Product(productName, productCost);
+                        listOfProducts.Add(product);
+                    }
+                    catch (ArgumentException ae)
+                    {
+                        Console.WriteLine(ae.Message);
+                        return;
+                    }
+                }
+            }
+
+            while (true)
+            {
+                string commandLine = Console.ReadLine();
+                if (commandLine == "END")
+                {
+                    break;
+                }
+                string[] tokens = commandLine.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                string name = tokens[0];
+                string product = tokens[1];
+
+                var currentPerson = listOfPeople.FirstOrDefault(x => x.Name == name);
+
+                var currentProduct = listOfProducts.FirstOrDefault(x => x.Name == product);
+
+                currentPerson.BuyProduct(currentProduct);
+               
+            }
+
+            foreach (var person in listOfPeople)
+            {
+                if (person.Products.Count == 0)
+                {
+                    Console.WriteLine($"{person.Name} - Nothing bought");
+                }
+                else
+                {
+                    Console.WriteLine($"{person.Name} - {String.Join(", ",person.Products)}");
+                }
+            }
+            
+        }
+    }
+}
