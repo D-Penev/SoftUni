76e5652942b34d0560938b1c46f9ed11f7c4c1cf
Mitko <predator131@mail.bg>
2020-02-28 15:21:25 +0200

    Upload exercise

---
+++ Shit for C#/C# OOP/Encapsulation Exercises/01 - Class Box Data/Engine.cs
@@ -0,0 +1,37 @@
+﻿namespace EncapsulationExercises
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class Engine
+    {
+        public void Run()
+        {
+            double first = double.Parse(Console.ReadLine());
+
+            double second = double.Parse(Console.ReadLine());
+
+            double third = double.Parse(Console.ReadLine());
+
+            
+
+            try
+            {
+                var box = new Box(first, second, third);
+                Console.WriteLine($"Surface Area - {box.CalculateSurfaceAreaa():f2}");
+                Console.WriteLine($"Lateral Surface Area - {box.CalculateLateralSurfaceAre():f2}");
+                Console.WriteLine($"Volume - {box.CalculateVolume():f2}");
+            }
+            catch (ArgumentException maikati)
+            {
+
+                Console.WriteLine(maikati.Message);
+            }
+
+            
+        }
+
+
+    }
+}
