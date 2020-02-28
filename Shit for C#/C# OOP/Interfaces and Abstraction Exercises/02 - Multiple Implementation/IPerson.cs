d8295bdb5bac32009a7c84b699170eac7f075bf4
Mitko <predator131@mail.bg>
2020-02-28 15:57:01 +0200

    Upload second exercise

---
+++ Shit for C#/C# OOP/Interfaces and Abstraction Exercises/02 - Multiple Implementation/IPerson.cs
@@ -0,0 +1,11 @@
+﻿namespace PersonInfo
+{
+    using System;
+    using System.Linq;
+    public interface IPerson
+    {
+        string Name { get; }
+
+        int Age { get; }
+    }
+}
---
+++ Shit for C#/C# OOP/Interfaces and Abstraction Exercises/02 - Multiple Implementation/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace PersonInfo
+{
+    using System;
+    using System.Linq;
+    public class StartUp
+    {
+        static void Main()
+        {
+            string personName = Console.ReadLine();
+            int personAge = int.Parse(Console.ReadLine());
+            string id = Console.ReadLine();
+            string birthdate = Console.ReadLine();
+            IBirthable birthable = new Citizen(personName, personAge, id, birthdate);
+            IIdentifiable identiable = new Citizen(personName, personAge, id, birthdate);
+            Console.WriteLine(identiable.Id);
+            Console.WriteLine(birthable.Birthdate);
+
+        }
+    }
+}
