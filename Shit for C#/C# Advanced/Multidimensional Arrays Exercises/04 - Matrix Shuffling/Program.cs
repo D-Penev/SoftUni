ef2478ea8ff2220554ed1c72d727d8944af23283
Mitko <predator131@mail.bg>
2020-02-28 16:46:14 +0200

    Uploaded multidimensional array exercise

---
+++ Shit for C#/C# Advanced/Multidimensional Arrays Exercises/04 - Matrix Shuffling/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MatrixShuffling
+{
+    class MatrixShuffling
+    {
+        static void Main()
+        {
+            int[] dimensions = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int rowSize = dimensions[0];
+            if (rowSize <= 0)
+            {
+                return;
+            }
+            var matrix = new string[rowSize][];
+            FillingMatrix(matrix);
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "END")
+                {
+                    break;
+                }
+                string[] tokens = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                if (tokens[0] != "swap")
+                {
+                    Console.WriteLine("Invalid input");
+                    continue;
+                }
+                if (tokens.Length > 5 || tokens.Length < 5)
+                {
+                    Console.WriteLine("Invalid input");
+                }
+                if (!Validation(tokens))
+                {
+                    Console.WriteLine("Invalid input");
+                }
+                int originalRow = int.Parse(tokens[1]);
+                int originalCol = int.Parse(tokens[2]);
+                int affectedRow = int.Parse(tokens[3]);
+                int affectedCol = int.Parse(tokens[4]);
+                if (IndexValidation(matrix, originalRow, originalCol, affectedRow, affectedCol))
+                {
+                    ElementShuffle(originalRow, originalCol, affectedRow, affectedCol, matrix);
+                    PrintMatrix(matrix);
+                }
+                else
+                {
+                    Console.WriteLine("Input is invalid");
+                }
+            }
+        }
+
+        private static void ElementShuffle(int originalRow, int originalCol, int affectedRow, int affectedCol, string[][] matrix)
+        {
+            var originalIndex = matrix[originalRow][originalCol];
+            var indexToExchange = matrix[affectedRow][affectedCol];
+            matrix[originalRow][originalCol] = indexToExchange;
+            matrix[affectedRow][affectedCol] = originalIndex;
+        }
+
+        private static void PrintMatrix(string[][] matrix)
+        {
+            foreach (var element in matrix)
+            {
+                Console.WriteLine(String.Join(" ", element));
+            }
+        }
+        private static void FillingMatrix(string[][] matrix)
+        {
+            for (int i = 0; i < matrix.Length; i++)
+            {
+                string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                if (input == null)
+                {
+                    Console.WriteLine("Input is invalid");
+                    return;
+                }
+                matrix[i] = new string[input.Length];
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    matrix[i][j] = input[j];
+                }
+            }
+        }
+        private static bool Validation(string[] tokens)
+        {
+            for (int i = 0; i < tokens.Length; i++)
+            {
+                if (tokens[i] == null)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        private static bool IndexValidation(string[][] matrix, int originalRow, int originalCol, int targetedRow, int targetedCol)
+        {
+            return originalRow >= 0 && originalRow < matrix.Length && originalCol >= 0 && originalCol < matrix[originalRow].Length && targetedRow >= 0 && targetedRow < matrix.Length && targetedCol >= 0 && targetedCol < matrix[targetedRow].Length;
+        }
+    }
+}
---
+++ Shit for C#/C# Advanced/Multidimensional Arrays Exercises/06 - Bombs/Program.cs
@@ -0,0 +1,87 @@
+﻿namespace Bombs
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+    public class Bombs
+    {
+        static void Main()
+        {
+            int rows = int.Parse(Console.ReadLine());
+            int[][] matrix = new int[rows][];
+            FillMatrix(rows, matrix);
+            string[] commands = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            ExplodeCells(commands, matrix);
+            var aliveCells = GetAliveCells(matrix);
+            Console.WriteLine($"Sum: {aliveCells.Sum()}");
+            Console.WriteLine($"Cells: {String.Join(" ", aliveCells)}");
+            PrintMatrix(matrix);
+
+
+        }
+
+        private static void ExplodeCells(string[] commands, int[][] matrix)
+        {
+            for (int i = 0; i < commands.Length; i++)
+            {
+                string[] tokens = commands[i].Split(",", StringSplitOptions.RemoveEmptyEntries);
+                int bombRow = int.Parse(tokens[0]);
+                int bombCol = int.Parse(tokens[1]);
+                int currentBombValue = matrix[bombRow][bombCol];
+                if (currentBombValue > 0)
+                {
+                    for (int row = bombRow - 1; row <= bombRow + 1; row++)
+                    {
+                        for (int col = bombCol - 1; col <= bombCol + 1; col++)
+                        {
+                            if (matrix[row][col] > 0)
+                            {
+                                if (IsInside(matrix, row, col))
+                                {
+                                    matrix[row][col] -= currentBombValue;
+                                }
+                            }
+                        }
+                    }
+                }
+
+
+            }
+        }
+        static bool IsInside(int[][] matrix, int row, int col)
+        {
+            return row >= 0 && row < matrix.Length && col >= 0 && col < matrix[row].Length;
+        }
+
+        static void FillMatrix(int rows, int[][] matrix)
+        {
+            for (int r = 0; r < rows; r++)
+            {
+                int[] inputData = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+                matrix[r] = inputData;
+            }
+        }
+        static void PrintMatrix(int[][] matrix)
+        {
+            foreach (var item in matrix)
+            {
+                Console.WriteLine(String.Join(" ", item));
+            }
+        }
+        static List<int> GetAliveCells(int[][] matrix)
+        {
+            var lista = new List<int>();
+            for (int r = 0; r < matrix.Length; r++)
+            {
+                for (int col = 0; col < matrix[r].Length; col++)
+                {
+                    if (matrix[r][col] > 0)
+                    {
+                        lista.Add(matrix[r][col]);
+                    }
+                }
+            }
+            return lista;
+        }
+    }
+}
